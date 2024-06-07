0f3f0bf77d68c93dc199ee5a19f292676ec80e47
Tyler Farrior <tylerfarrior@gmail.com>
2024-06-07 17:24:33 -0400

    Add sanity package

---
+++ packages/karasu-sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wexq7piw',
+    dataset: 'production'
+  }
+})
---
+++ packages/karasu-sanity/schemaTypes/heroimages.ts
@@ -0,0 +1,18 @@
+export default {
+    name: 'heroImage',
+    type: 'document',
+    title: 'Two Hero Image',
+    fields: [
+        {
+            name: 'image1',
+            type: 'image',
+            title: 'First image',
+        },
+        {
+            name: 'image2',
+            type: 'image',
+            title: 'Second Image',
+        },
+
+    ],
+}
