f3af1d8ed0849d514e49c76c5b4a55c706ba2569
Tyler Farrior <tylerfarrior@gmail.com>
2024-06-20 19:21:38 -0400

    fixing bugs in layout.tsx

--- apps/web-app/src/app/layout.tsx
+++ apps/web-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "./Navbar";
+import Navbar from './Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
