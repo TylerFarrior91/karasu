3806ff3d087a3983d8a438d291d59b9b68b8ef4e
Tyler Farrior <tylerfarrior@gmail.com>
2024-07-03 17:49:45 -0400

    creating the UI

--- apps/web-app/src/app/Navbar.tsx
+++ apps/web-app/src/app/Navbar.tsx
@@ -1,7 +1,13 @@
 export default function Navbar() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <header className="mb-8 border-b">
+            <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+                <link href="/">
+                    <h1 className="text-2xl font-bold"></h1>
+
+                </link>
+            </div>
+
+        </header>
     );
 }
