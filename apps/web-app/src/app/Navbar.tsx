3fc071f4e670435354d0c56f89f651aa3f4da401
Tyler Farrior <tylerfarrior@gmail.com>
2024-07-04 17:50:35 -0400

    fixing navbar

--- apps/web-app/src/app/Navbar.tsx
+++ apps/web-app/src/app/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 
+const links = {
+    
+}
 export default function Navbar() {
     return (
         <header className="mb-8 border-b">
             <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
                 <link href="/">
-                    <h1 className="text-2xl font-bold"></h1>
+                    <h1 className="text-5xl font-bold">
+                        Next<span className="text-primary">Katasu</span>
+                    </h1>
 
                 </link>
             </div>
